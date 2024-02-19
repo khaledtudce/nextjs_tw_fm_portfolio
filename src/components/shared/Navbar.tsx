48e3b11c495a5fb44cc6b2cc027ebe395883b0a6
Khaled Reza <reza@lorop.de>
2024-02-19 23:16:27 +0100

    added simple animation on Hamburger menu

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -50,17 +50,17 @@ const Navbar = () => {
         onClick={handleOpenClose}
       >
         <span
-          className={`bg-dark dark:bg-light block h-0.5 w-6 rounded-sm ${
+          className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${
             isOpen ? "rotate-45 translate-y-1" : "-translate-y-0.5"
           }`}
         ></span>
         <span
-          className={`bg-dark dark:bg-light block h-0.5 w-6 rounded-sm m-0.5 ${
+          className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm m-0.5 ${
             isOpen ? "opacity-0" : "opacity-100"
           }`}
         ></span>
         <span
-          className={`bg-dark dark:bg-light block h-0.5 w-6 rounded-sm  ${
+          className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm  ${
             isOpen ? "-rotate-45 -translate-y-1" : "translate-y-0.5"
           }`}
         ></span>
