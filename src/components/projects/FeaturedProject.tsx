18664c2d7df61e92753e6f1da82f6ace1fb75d1e
Khaled Reza <reza@lorop.de>
2024-02-19 22:29:09 +0100

    Did image optimization using priority and sizes element

--- src/components/projects/FeaturedProject.tsx
+++ src/components/projects/FeaturedProject.tsx
@@ -36,6 +36,8 @@ export const FeaturedProject = ({
           className="w-full h-auto"
           whileHover={{ scale: 1.05 }}
           transition={{ duration: 0.2 }}
+          priority
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
         />
       </Link>
       <div className="w-1/2 flex flex-col items-start justify-between pl-6">
--- src/components/projects/Project.tsx
+++ src/components/projects/Project.tsx
@@ -29,6 +29,8 @@ export const Project = ({ title, type, img, link, github }: ProjectProps) => {
           className="w-full h-auto"
           whileHover={{ scale: 1.05 }}
           transition={{ duration: 0.2 }}
+          priority
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
         />
       </Link>
       <div className="w-full flex flex-col items-start justify-between mt-4">
